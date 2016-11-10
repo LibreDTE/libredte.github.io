47a91bf07eb974713fb6f03c9e93f7278a0eb09b
Esteban De La Fuente Rubio <esteban@delaf.cl>
2016-11-10 00:31:59 -0300

    Se publica nuevo sitio web proyecto LibreDTE

---
+++ assets/js/feedback.js
@@ -0,0 +1,23 @@
+var feedback = [
+	// fecha, nombre, email, empresa, cliente, mostrar, comentario
+	["2016-05-04", "Raúl Monsalves", "estranged1977@gmail.com", "", "", "1", "Nosotros empezamos a ocupar LibreDTE hace poco, es tremenda plataforma. Tenemos otros dos desarrollos pero esto es mucho mas completo."],
+	["2016-04-28", "Ricardo Rosales", "gerencia@tiempodemotos.cl", "Tiempo de motos", "1", "1", "Te felicito por la aplicación a ti y al equipo de trabajo que está en LibreDTE, muy buena y el precio por el servicio está muy ad hoc"],
+	["2016-03-14", "Enzo Ramírez", "eramirez@frmaestranza.cl", "FR Maestranza", "1", "1", "100% recomendable!!! Atención de 1ra!!"],
+	["2016-03-03", "Johnny Verdugo", "jverdugo@linets.cl", "Linets", "", "1", "LibreDTE nos ayudó a mejorar toda la plataforma de facturación a través de su librería, nos aclaró dudas, conceptos por sobre todo nos dieron una atención de calidad, recomendado 100%"],
+	["2016-02-22", "Felipe Rosales", "felipe_arg@hotmail.com", "Mi Programa Ltda", "1", "1", "Damos los agradecimientos por el desarrollo entregado por LibreDTE, ya que con esta herramienta en conjunto con su experiencia y excelente disposición se ha logrado mucho más de lo que se había avanzando en los últimos 6 meses de investigación."],
+	["2016-02-01", "Alfredo Concha", "alfredo@gesnex.com", "GesNex", "", "1", "Ya están provocando que la competencia me redujera su cotización a la mitad."],
+	["2015-06-06", "Patricio Díaz", "webmaster@gnulinux.cl", "FacTrónica", "", "1", "Su rutina de digest publicada en la web fue uno de los procesos engorrosos de realizar, pero con esa rutina salí perfecto de ese punto."],
+	["2016-01-05", "Cristian Droguett", "cristian@cebra.cl", "Cebra SpA", "", "1", "Gracias por poner a disposición su gran trabajo, hemos instalado el cliente web en nuestro servidor sin problemas."],
+	["2015-12-16", "Rodrigo Gonzalez", "rodrigo.gonzalez@gsbpo.cl ", "Gs BPO", "", "1", "Muchas gracias por toda la ayuda prestada."],
+	["2016-02-16", "Rodrigo Jimenez", "rjimenez@inputsoft.cl", "Inputsoft", "", "1", "He ocupado la herramienta y he tenido éxito, es un gran aporte."],
+	["2016-03-06", "Raul Salamanca Herrera", "ralsalamanca@gmail.com", "", "", "1", "Felicitaciones por el proyecto. Esto es un gran aporte para las PyMEs y emprendedores, porque les facilita bastante el proceso de la facturación electrónica."],
+	["2016-02-23", "Nelson Agurto", "nelsonagurtop@gmail.com", "", "", "1", "Los felicito por su proyecto, es genial, he aprendido mucho acá. También he hecho mis humildes aportes en los foros, gran sitio. A pesar que programo en .NET, la información detallada y específica de los foros se agradece. Un gran aporte."],
+	["2016-02-19", "Maximiliano", "max_delphi@hotmail.com", "", "", "1", "Quiero agradecer a su equipo por el desarrollo de LibreDTE, estoy desarrollando un sistema para facturación y LibreDTE me ha guiado en mucho. Les agradezco sinceramente por toda su ayuda y colaboración a la comunidad. ¡Gracias LibreDTE!"],
+	["2015-11-11", "Juan Francisco Rodríguez", "zenilt@gmail.com", "", "", "1", "La librería está súper buena."],
+	["2015-12-15", "Luis Paillao", "chalao.lp@gmail.com", "", "", "1", "Agradecer por el desarrollo de estas librerías."],
+	["2015-11-18", "Oscar Tolaq", "oscar.tola@gmail.com", "", "", "1", "Felicitarlos por esta iniciativa de compartir con la comunidad con una librería que de seguro tendrá mucho éxito."],
+	["2015-09-30", "Herman Vega", "hvegax@gmail.com", "", "", "1", "Me pareció interesante su proyecto LibreDTE, sin duda un gran aporte a la comunidad de software libre."],
+	["2015-10-29", "Francisco Javier", "francisco.cpp@gmail.com", "", "", "1", "Felicitaciones por su iniciativa."],
+	["2015-10-07", "Ricardo Rebolledo", "rebocano@gmail.com", "", "", "1", "Encuentro magnífica la idea."],
+	["2015-09-27", "Patricio", "psazop@gmail.com", "", "", "1", "Parece un excelente proyecto, felicitaciones."]
+];
